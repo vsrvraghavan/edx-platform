50632d9f8345536b25b7094887162186166f1929
raghuver <vsrv.raghavan@gmail.com>
2020-12-25 15:35:59 +0530

    added migrations

---
+++ custom-form-app/custom_reg_form/migrations/0001_initial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='UserProfileExtraInfo',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('favorite_language', models.CharField(max_length=100, verbose_name=b'Favorite Programming Language', error_messages={b'required': 'Please tell us your programming language.', b'invalid': "It's an invalid entry."})),
+                ('favorite_editor', models.CharField(blank=True, max_length=5, verbose_name=b'Favorite Editor', choices=[(b'vim', b'Vim'), (b'emacs', b'Emacs'), (b'bbedit', b'BBEdit'), (b'np', b'Notepad'), (b'cat', b'cat > filename')])),
+                ('user', models.OneToOneField(null=True, to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)),
+            ],
+            options={
+                'db_table': 'auth_userprofile_extrainfo',
+        ),
+    ]
