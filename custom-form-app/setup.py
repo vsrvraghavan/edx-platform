36b805504e82a703e988b5933dbc284536c666d2
raghuver <vsrv.raghavan@gmail.com>
2021-03-24 04:33:30 +0530

    Update setup.py

--- custom-form-app/setup.py
+++ custom-form-app/setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 # Main ##############################################################
 
 setup(
-    name='custom-form-app',
+    name='custom_reg_form',
     version='1.0',
     description='LMS - Custom Registration Extension Form',
     packages=['custom_reg_form'],
