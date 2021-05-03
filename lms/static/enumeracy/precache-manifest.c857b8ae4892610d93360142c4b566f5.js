6dd19a1e4dbb12659f1b90a150800e17a2070b18
raghuver <vsrv.raghavan@gmail.com>
2021-05-03 13:59:50 +0530

    Added enumeracy (personalisation) react client side files

---
+++ lms/static/enumeracy/precache-manifest.c857b8ae4892610d93360142c4b566f5.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5de7a109d58bcf1365bea7c49cdfd89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4e52a1eb3176cb88264",
+    "url": "/static/css/main.bcafe8db.chunk.css"
+  },
+  {
+    "revision": "194c91f039c6e3a9bbf5",
+    "url": "/static/js/2.f028ba7f.chunk.js"
+  },
+  {
+    "revision": "c4e52a1eb3176cb88264",
+    "url": "/static/js/main.e9458796.chunk.js"
+  },
+  {
+    "revision": "883f137dfe838c37949c",
+    "url": "/static/js/runtime~main.96b76cd5.js"
+  },
+  {
+    "revision": "1088b4a18bfb0868593ff1ca03675d23",
+    "url": "/static/media/Muli-Bold.1088b4a1.svg"
+  },
+  {
+    "revision": "1e36ee6d50c037b1bb4bdd3effa7a28e",
+    "url": "/static/media/Muli-Bold.1e36ee6d.ttf"
+  },
+  {
+    "revision": "5f917beacdaf8c5384b0196ed1b94d07",
+    "url": "/static/media/Muli-Bold.5f917bea.eot"
+  },
+  {
+    "revision": "dd91ee6e8b24c425e335531c91447385",
+    "url": "/static/media/Muli-Bold.dd91ee6e.woff"
+  },
+  {
+    "revision": "847da9dd2ffad3df86fca7bf7ed4ed2a",
+    "url": "/static/media/Muli.847da9dd.svg"
+  },
+  {
+    "revision": "8e79b05b2e59845d3feb16321c0ee41e",
+    "url": "/static/media/Muli.8e79b05b.woff"
+  },
+  {
+    "revision": "a116c44e16ea8d8b0a70cb3d6d3f4f89",
+    "url": "/static/media/Muli.a116c44e.eot"
+  },
+  {
+    "revision": "df7330254513d2fa2f4c1e9ee98cc6c6",
+    "url": "/static/media/Muli.df733025.ttf"
+  },
+  {
+    "revision": "9ca8136e4bed1fe993781c8274eb5d71",
+    "url": "/static/media/calc-scale.9ca8136e.png"
+  },
+  {
+    "revision": "1889dfc18e77a35ab8ce1484967cedea",
+    "url": "/static/media/chat-screen-background.1889dfc1.png"
+  },
+  {
+    "revision": "8fd53c3f54022bd577db34b2330ca3ae",
+    "url": "/static/media/coin-counts@2x.8fd53c3f.png"
+  },
+  {
+    "revision": "2d3121a4fc87d638b30f2a2824d5c128",
+    "url": "/static/media/congratulation-completion-natural-numbers@2x.2d3121a4.png"
+  },
+  {
+    "revision": "9372c3e71b697d568ceca1ceb3a509b6",
+    "url": "/static/media/congratulations-quiz-completion@2x.9372c3e7.png"
+  },
+  {
+    "revision": "6f5938d517a3f07bdddd845483ef742b",
+    "url": "/static/media/good-effort-graphic@2x.6f5938d5.png"
+  },
+  {
+    "revision": "45dba139934456bb30775467fa40c701",
+    "url": "/static/media/iPadPro-screen-background.45dba139.png"
+  },
+  {
+    "revision": "82ff33ba2f546fb4be0648469aae6186",
+    "url": "/static/media/steve.82ff33ba.png"
+  },
+  {
+    "revision": "c0c6fa0ddb7da3788a5a7a3e2e4fd868",
+    "url": "/static/media/traingle-pencil.c0c6fa0d.png"
+  },
+  {
+    "revision": "2b1799740def3eb50c3c3f9f9ab7873b",
+    "url": "/static/media/uncle-steve-porfile.2b179974.png"
+  },
+  {
+    "revision": "1dd1a9a96cf652b9e8c9737c776d1ab7",
+    "url": "/static/media/well-done-graphic@2x.1dd1a9a9.png"
+  }
+]);
